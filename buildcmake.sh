f68b816dd154999e03863616bf1b2b35687d5a09
Epimetheus <epimetheus@swordintelligence.airforce>
2025-05-19 14:04:32 +0100

    Update buildcmake.sh

--- buildcmake.sh
+++ buildcmake.sh
@@ -1,11 +1,13 @@
-# 1. Download source
+# 1. Download & unpack
 wget https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1.tar.gz
 tar -xzf cmake-3.30.1.tar.gz
 cd cmake-3.30.1
 
-# 2. Bootstrap & build
+# 2. Bootstrap & build with 20 jobs and -march=native
 ./bootstrap -- -DCMAKE_USE_OPENSSL=ON
-make -j$(nproc)
+make -j20 \
+     CFLAGS="-O3 -march=native" \
+     CXXFLAGS="-O3 -march=native"
 
 # 3. Install
 sudo make install
